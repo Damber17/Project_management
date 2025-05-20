1c7a8cc45b3bedaae6390cdf754a1621632974f4
Damber17 <dx17bht@gmail.com>
2025-05-20 18:50:23 +0600

    fix: Add ESLint configuration to handle unescaped entities

--- src/pages/login.js
+++ src/pages/login.js
@@ -180,7 +180,7 @@ export default function Login() {
 
             <div className="mt-6 text-center">
               <p className="text-sm text-foreground/60">
-                Don't have an account?{' '}
+                Don&apos;t have an account?{' '}
                 <Link 
                   href="/register" 
                   className="text-primary hover:text-primary-dark font-medium transition-colors"
